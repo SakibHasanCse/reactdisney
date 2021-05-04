4da0cc0b4ca274835d9eaa40ef0cc99f15cb05b2
SakibHasanCse <sakibhasan9641@gmail.com>
2021-05-04 23:15:04 +0600

    update Router

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Details from './components/Details';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail" >
+            <Details />
+          </Route>
+          <Route path="/">
+
+            <Home />
+          </Route>
+        </Switch>
+
+      </Router>
     </div>
 
   );
