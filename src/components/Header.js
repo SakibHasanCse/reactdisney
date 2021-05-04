1658560713d7a1d531333fe0e394ecae7024ab36
SakibHasanCse <sakibhasan9641@gmail.com>
2021-05-04 22:07:25 +0600

    ADD : image slider

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+    return (
+        <Nav>
+            <Logo src="/images/logo.svg" />
+            <NavMenu >
+                <a>
+                    <img src="/images/home-icon.svg" alt="" />
+                    <span>HOME</span>
+                </a>
+                <a>
+                    <img src="/images/search-icon.svg" alt="" />
+                    <span>SEARCH</span>
+                </a> <a>
+                    <img src="/images/watchliist-icon.svg" alt="" />
+                    <span>WATCHLIST</span>
+                </a> <a>
+                    <img src="/images/original-icon.svg" alt="" />
+                    <span>ORIGINALS</span>
+                </a>
+                <a>
+                    <img src="/images/movie-icon.svg" alt="" />
+                    <span>MOVIES</span>
+                </a>
+                <a>
+                    <img src="/images/series-icon.svg" alt="" />
+                    <span>SERIES</span>
+                </a>
+            </NavMenu>
+            <UserImage src="https://scontent.fdac5-2.fna.fbcdn.net/v/t1.6435-1/p320x320/86264389_1066822800317772_2323266682216251392_n.jpg?_nc_cat=107&ccb=1-3&_nc_sid=7206a8&_nc_ohc=bV54YhlY5RoAX8D9IEj&_nc_ht=scontent.fdac5-2.fna&tp=6&oh=47c88d09d174885dc42365dc36475c08&oe=60B682C8" />
+
+        </Nav>
+    )
+}
+
+const Nav = styled.div`
+    height:70px;
+    background:#090b13;
+    display:flex;
+    align-items:center;
+    padding:0px 36px;
+    overflow-x: hidden;
+`
+const Logo = styled.img`
+    width:80px ; 
+  
+`
+const NavMenu = styled.div`
+    display:flex;
+    flex:1;
+   margin-left:25px;
+   align-items:center;
+    a{
+        display:flex;       
+        align-items:center;
+        padding:0px 12px;
+        cursor: pointer;
+       
+
+        img{
+            height:20px;
+
+        }
+        span{
+            font-size:13px;
+            letter-spacing:1.42;
+            position:relative;
+
+            &:after{
+                content:"";
+                background:white;
+                height:2px;
+                position:absolute;
+                right:0;
+                left:0;
+                bottom:-6px;
+                opacity:0;
+                transform-origin:left center;
+                transition:all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform:scaleX(0)
+            }
+
+        }
+        &:hover{
+            span:after{
+                transform:scaleX(1);
+                opacity:1
+
+            }
+        }
+    }
+`
+const UserImage = styled.img`
+    height:48px;
+    width:48px;
+    border-radius:50%;
+
+`
+
+export default Header
+
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from 'styled-components';
+
+const ImageSlider = () => {
+     var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+         slidesToScroll: 1,
+    autoplay: true
+  };
+
+    return (
+        <Carousel {...settings}>
+            <Wrap>
+                <img src="/images/slider-badging.jpg" alt="image-slider"/>
+            </Wrap>
+             <Wrap>
+                <img src="/images/slider-badag.jpg" alt="image-slider"/>
+            </Wrap>
+            <Wrap>
+                <img src="/images/slider-badag.jpg" alt="image-slider"/>
+            </Wrap>
+
+
+        </Carousel>
+
+    );
+}
+
+
+
+export default ImageSlider;
+
+const Carousel = styled(Slider)`
+margin-top:20px;
+
+ul li button{
+    &:before{
+        font-size:10px;
+        color:rgb(150 ,158, 171)
+    }
+}
+li.slick-active button::before{
+    color:white 
+}
+button{
+    z-index:1;
+}
+        .slick-list{
+            overflow:visible;
+        }
+
+`
+const Wrap = styled.div`
+cursor:pointer;
+    img{
+        border:4px solid transparent;
+        border-radius:4px;
+        height:100%;
+        width:100%;
+        box-shadow:rgb(0 0 0 /69%) 0px 26px 30px -10px,
+        rgb(0 0 0 /73%) 0px 16px 10px -10px;
+        transition-duration:300ms;
+
+        &:hover{
+            border:3px solid #f0e7e7
+        }
+    }
+
+
+`
