423ad5f7a306f0616f451f01be37d5ac178b0c99
SakibHasanCse <sakibhasan9641@gmail.com>
2021-05-06 22:58:14 +0600

    add

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styled from 'styled-components'
 
 const Details = () => {
     return (
-        <div>
+        <Container>
             details page
-        </div>
+        </Container>
     );
 }
 
 export default Details;
+
+const Container = styled.div`
+    min-height:calc(100vh - 70px);
+    padding:0 calc(3.5vw + 5px);
+
+`
